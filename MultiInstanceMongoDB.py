fb63127fbfaec8a67c1809d8cf02a5f867c77fa7
Mark Sullivan <mark@sullivans.id.au>
2025-08-11 14:51:21 +1000

    Add flow index insert

--- MultiInstanceMongoDB.py
+++ MultiInstanceMongoDB.py
@@ -4,6 +4,7 @@ This plugin uses MongoDB to filter commands across multiple instances of Errbot.
 It ensures that only the first instance that receives a command will execute it,
 while later instances will assume the command has already been executed.
 """
+
 import uuid
 from datetime import datetime
 from datetime import timezone
@@ -32,21 +33,19 @@ class MultiInstanceMongoDBPlugin(BotPlugin):
     def activate(self):
         super().activate()
 
-        mongo_uri =  getattr(self.bot_config, MONGODB_URI, None)
+        mongo_uri = getattr(self.bot_config, MONGODB_URI, None)
         if not mongo_uri:
-            raise ValueError(
-                    f"{MONGODB_URI} must be set in the bot configuration."
-            )
+            raise ValueError(f"{MONGODB_URI} must be set in the bot configuration.")
 
         parsed = parse_uri(mongo_uri)
         collection = parsed.get("collection")
 
         ttl = getattr(self.bot_config, MONGODB_INDEX_TTL, 30)
-        ttl_flow = getattr(self.bot_config, MONGODB_INDEX_FLOW_TTL, 60*5)
+        ttl_flow = getattr(self.bot_config, MONGODB_INDEX_FLOW_TTL, 60 * 5)
 
         if not collection:
             raise ValueError(
-                    f"{MONGODB_URI} must specify both database and collection, e.g. /<db>.<collection>"
+                f"{MONGODB_URI} must specify both database and collection, e.g. /<db>.<collection>"
             )
 
         self.mongo_client = MongoClient(mongo_uri)
@@ -57,23 +56,32 @@ class MultiInstanceMongoDBPlugin(BotPlugin):
             self.collection = db[collection]
 
         for idx in self.collection.list_indexes():
-            if idx.get('name') == INDEX_NAME:
-                if idx.get('expireAfterSeconds') != ttl:
+            if idx.get("name") == INDEX_NAME:
+                if idx.get("expireAfterSeconds") != ttl:
                     self.collection.drop_index(INDEX_NAME)
-                    self.collection.create_index("datetime", expireAfterSeconds=ttl, name=INDEX_NAME)
+                    self.collection.create_index(
+                        "datetime", expireAfterSeconds=ttl, name=INDEX_NAME
+                    )
                 break
         else:
-            self.collection.create_index("datetime", expireAfterSeconds=ttl, name=INDEX_NAME)
+            self.collection.create_index(
+                "datetime", expireAfterSeconds=ttl, name=INDEX_NAME
+            )
 
         for idx in self.collection.list_indexes():
-            if idx.get('name') == INDEX_NAME_FLOW:
-                if idx.get('expireAfterSeconds') != ttl_flow:
+            if idx.get("name") == INDEX_NAME_FLOW:
+                if idx.get("expireAfterSeconds") != ttl_flow:
                     self.collection.drop_index(INDEX_NAME_FLOW)
-                    self.collection.create_index("datetime_flow", expireAfterSeconds=ttl_flow, name=INDEX_NAME_FLOW)
+                    self.collection.create_index(
+                        "datetime_flow",
+                        expireAfterSeconds=ttl_flow,
+                        name=INDEX_NAME_FLOW,
+                    )
                 break
         else:
-            self.collection.create_index("datetime_flow", expireAfterSeconds=ttl_flow, name=INDEX_NAME_FLOW)
-
+            self.collection.create_index(
+                "datetime_flow", expireAfterSeconds=ttl_flow, name=INDEX_NAME_FLOW
+            )
 
     def deactivate(self):
         """
@@ -105,15 +113,19 @@ class MultiInstanceMongoDBPlugin(BotPlugin):
         # will be considered to be the same command and will not be executed again.
         # To avoid this, the backend should inject a unique message id into the
         # "extras" dict of the message.
-        message_id = msg.extras.get("message_id") or f"{msg.body}|{msg.frm}|{msg.to}|{cmd}|{args}".encode("utf-8")
+        message_id = msg.extras.get(
+            "message_id"
+        ) or f"{msg.body}|{msg.frm}|{msg.to}|{cmd}|{args}".encode("utf-8")
 
         # The flow is only attached to a message AFTER the flow has progressed
         # past the filtering. So we need to do some pre-checks to determine if
         # the command will trigger a new flow or is part of an existing flow
-        flow_inflight, _ = self._bot.flow_executor.check_inflight_flow_triggered(cmd, msg.frm)
+        flow_inflight, _ = self._bot.flow_executor.check_inflight_flow_triggered(
+            cmd, msg.frm
+        )
 
         if flow_inflight:
-            flow_root=flow_inflight.flow_root
+            flow_root = flow_inflight.flow_root
 
         if not flow_root:
             # is the command going to trigger a new flow?
@@ -121,7 +133,7 @@ class MultiInstanceMongoDBPlugin(BotPlugin):
 
         if flow_root:
 
-            flow_find = self.collection.find_one({"flow_root"  : flow_root})
+            flow_find = self.collection.find_one({"flow_root": flow_root})
 
             if flow_find:
                 if flow_find.get("instance_id") == self.instance_id:
@@ -131,22 +143,33 @@ class MultiInstanceMongoDBPlugin(BotPlugin):
                     # another instance has already started with the flow, so we should not run with it
                     return None, None, None
 
+            try:
+                self.collection.insert_one(
+                    {
+                        "_id": message_id,
+                        "instance_id": self.instance_id,
+                        "flow_root": flow_root,
+                        "datetime_flow": datetime.now(timezone.utc),
+                    }
+                )
+                return msg, cmd, args
+
+            except DuplicateKeyError:
+                return None, None, None
 
         try:
             self.collection.insert_one(
-                    {
-                        "_id"        : message_id,
-                        "instance_id": self.instance_id,
-                        "flow_root"  : flow_root,
-                        "datetime"   : datetime.now(timezone.utc),
-                    }
+                {
+                    "_id": message_id,
+                    "instance_id": self.instance_id,
+                    "datetime": datetime.now(timezone.utc),
+                }
             )
 
         except DuplicateKeyError:
             return None, None, None
 
         return msg, cmd, args
-
 
     @botcmd
     def show_instance_id(self, _msg, _args):
